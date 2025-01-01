aa650c64e9b70c8d0a596da7248272bd9ebccef3
Akash Jain <ajcodes.tech@gmail.com>
2025-01-01 18:23:29 +0000

    updated gagc command

--- package/aliases.sh
+++ package/aliases.sh
@@ -1,5 +1,5 @@
 alias j23='export PATH="$PATH:~/.java/OpenJDK-23/bin"' 
 alias gc='git checkout'
 alias gcm='git checkout main'
-alias gagc='git add . && git commit -m'
+alias gagc='git add . && git commit'
 
